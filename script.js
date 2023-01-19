9b30d362df8fab5804eb64c5ebd13dd5d3ab629c
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-01-19 16:47:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	data = {0:0, 1:1, 2:abc, 3:def, 4:ghi, 5:jkl, 6:mno, 7:pqrs,8:tuv,9:wxyz}
+	for (key of data) {
+		if(input_digit == key){}
+	}
 }
 
 module.exports = letterCombinations;
