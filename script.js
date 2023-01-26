495ff683726660db022812619cbf2a4dbdd52123
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-01-26 09:12:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 function letterCombinations(input_digit) {
   //Complete the function
-	data = {0:0, 1:1, 2:abc, 3:def, 4:ghi, 5:jkl, 6:mno, 7:pqrs,8:tuv,9:wxyz}
-	for (key of data) {
-		if(input_digit == key){}
-	}
+    if(input_digits == ""){
+        return [];
+    }
+    let table = [ '0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+     
+    let res =[];
+    let que = [''];
+     
+    while(que.length>0){
+        let str = que[0];
+        que.shift();
+         
+        if(str.length == input_digits.length){
+            res.push(str);
+        } else{
+            let s= Number(input_digits.charAt(str.length));
+            let val = table[s];
+             
+            for(i=0;i<val.length;i++){
+                que.push(str+val.charAt(i));
+            }
+        }
+    }
+     
+    return res;
 }
 
 module.exports = letterCombinations;
